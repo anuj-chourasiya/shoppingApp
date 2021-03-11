089a2356c1899ff2fe76d359dc8d08da26a6d3c0
Anuj Chourasiya <anujchourasiyadp@gmail.com>
2021-03-11 16:14:05 +0530

    remove cart

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -14,7 +14,7 @@ def	cart_add(request,	product_id):
         cart.add(product=product,quantity=cd['quantity'],update_quantity=cd['update'])
     return	redirect('cart:cart_detail')
 
-def	cart_remove(request,	product_id):
+def	cart_remove(request,product_id):
     cart	=	Cart(request)
     product	=	get_object_or_404(Product,	id=product_id)
     cart.remove(product)
